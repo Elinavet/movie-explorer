235f619be49e933f3fcc3da72442e2c69cf90a62
Elinavet <freeefauna@gmail.com>
2026-09-24 20:55:17 +0100

    Handle missing movie data

--- script.js
+++ script.js
@@ -20,22 +20,38 @@ const searchMovies = async (query)=>{
 
   const data = await response.json()
   movieResults.innerHTML = "";
-  data.results.forEach(movie => {
-    const movieCard = document.createElement("div")
-    const movieTitle = document.createElement("h2");
-    movieTitle.innerText = movie.title
-    const movieDate = document.createElement("p");
-    movieDate.innerText = movie.release_date.slice(0, 4)
-    const movieRating = document.createElement("p");
-    movieRating.innerText = `⭐ ${movie.vote_average.toFixed(1)}`
-    const moviePoster = document.createElement("img");
-    moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    
-    movieCard.appendChild(moviePoster)
-    movieCard.appendChild(movieRating)
-    movieCard.appendChild(movieTitle);
-    movieCard.appendChild(movieDate);
-    movieResults.appendChild(movieCard)
-  });
+
+  if (data.results.length === 0) {
+    movieResults.innerText = "No movies found.";
+  } else {
+    data.results.forEach(movie => {
+      const movieCard = document.createElement("div")
+      const movieTitle = document.createElement("h2");
+      movieTitle.innerText = movie.title
+      const movieDate = document.createElement("p");
+      if (movie.release_date !== "") {
+        movieDate.innerText = movie.release_date.slice(0, 4);
+      } else {
+        movieDate.innerText = "Release date unknown";
+      }
+
+      const movieRating = document.createElement("p");
+      movieRating.innerText = `⭐ ${movie.vote_average.toFixed(1)}`
+      
+      const moviePoster = document.createElement("img");
+      if (movie.poster_path !== null) {
+        moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;  
+      }else{
+        moviePoster.src = ".//images/img placeholder.png"
+      }
+      
+      movieCard.appendChild(moviePoster)
+      movieCard.appendChild(movieRating)
+      movieCard.appendChild(movieTitle);
+      movieCard.appendChild(movieDate);
+      movieResults.appendChild(movieCard)
+    });
+  }
+  
 }
 
